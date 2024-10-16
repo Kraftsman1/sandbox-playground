5c84f1ce0b794d0f8df5d97b14a19c482e694ce5
Kraftsman <shamsasigri@gmail.com>
2024-10-16 21:35:17 +0000

    add solution for spilt string

--- codewars/isIsomorphic.php
+++ codewars/isIsomorphic.php
@@ -49,4 +49,24 @@ echo areIsomorphic('foo', 'bar') ? 'True' : 'False'; // Output: False
 echo "\n";
 echo areIsomorphic('paper', 'title') ? 'True' : 'False'; // Output: True
 
+
+// split a given string into pairs of two characters. 
+// If the string contains an odd number of characters then it should replace 
+// the missing second character of the final pair with an underscore ('_').
+function split_pairs($str) {
+    // If the string length is odd, append an underscore
+    if (strlen($str) % 2 != 0) {
+        $str .= '_';
+    }
+    
+    // Split the string into pairs of two characters
+    return str_split($str, 2);
+}
+
+// Example usage:
+print_r(split_pairs('abc'));  // Output: Array ( [0] => ab [1] => c_ )
+print_r(split_pairs('abcd')); // Output: Array ( [0] => ab [1] => cd )
+
+
+
 ?>
